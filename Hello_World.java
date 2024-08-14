e880dfdb2db35efeffc3f66cde82e02f713a6bd6
sleepynapz <scalicdan2025@damien.edu>
2024-08-14 10:30:35 -1000

    Create Hello_World.java

---
+++ Hello_World.java
@@ -0,0 +1,16 @@
+/*
+ * This makes me feel like one of them hackers from the shows, but instead it's not that intense.
+ */
+public class Hello_World {
+
+	public static void main(String[] args) {
+		
+		//Task 1: Print out Hello World!
+		//Task 2: Print Hello World into the next line
+		System.out.print("Hello World!");
+		System.out.println("Hello World!");
+		System.out.print("Hello World!");
+		
+	}
+
+}
